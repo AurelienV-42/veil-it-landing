48b64a752441eb320cf8718a87cf88bb03ddd2c4
aurelienv <aurelienvpro@gmail.com>
2025-06-23 01:31:40 +0200

    refactor: remove unused sections to streamline the homepage
    - Deleted ProblemSection, SimpleWhyChoose, and SolutionSteps components to simplify the homepage structure.
    - Updated the Home component to reflect these changes, enhancing maintainability and readability.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,8 @@
 import TopNavigation from '@/components/TopNavigation';
 import FAQ from '@/sections/FAQ';
 import FinalCTA from '@/sections/FinalCTA';
-import ProblemSection from '@/sections/ProblemSection';
 import SimpleFooter from '@/sections/SimpleFooter';
 import SimpleHero from '@/sections/SimpleHero';
-import SimpleWhyChoose from '@/sections/SimpleWhyChoose';
-import SolutionSteps from '@/sections/SolutionSteps';
 
 export default function Home() {
   return (
@@ -13,15 +10,6 @@ export default function Home() {
       <TopNavigation />
       <div id="hero">
         <SimpleHero />
-      </div>
-      <div id="problem">
-        <ProblemSection />
-      </div>
-      <div id="solution">
-        <SolutionSteps />
-      </div>
-      <div id="features">
-        <SimpleWhyChoose />
       </div>
       <div id="faq">
         <FAQ />
